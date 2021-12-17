82ac2138b7f2e4631a0bd2aefc4149ff29a98881
Hector <1240253702@qq.com>
2021-12-17 22:45:10 +0800

    Update orchestration.sh

--- orchestration.sh
+++ orchestration.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-apt install -y ansible sshpass && ansible-playbook -i hosts.yml ssh_setup.yml && ansible-playbook -i hosts.yml reinitCluster.yml
+apt install -y ansible sshpass && export ANSIBLE_HOST_KEY_CHECKING=false && ansible-playbook -i hosts.yml ssh_setup.yml && ansible-playbook -i hosts.yml reinitCluster.yml
+ 
